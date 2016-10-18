6c1017f4b6c7399167153e9ba0ea4a47e7db42c5
marlodam <marlodam@greeny.cs.tlu.ee>
2016-10-18 22:23:16 +0300

    täiendasin

---
+++ 2.kodutoo-I-ruhm/data.php
@@ -0,0 +1,85 @@
+<?php 
+	require("functions.php");
+	
+	// 2.kodutöö
+	// kas on sisseloginud, kui ei ole siis
+	// suunata login lehele
+	if (!isset ($_SESSION["userId"])) {
+		
+		header("Location: login.php");
+		
+	}
+	
+	//kas ?logout on aadressireal
+	if (isset($_GET["logout"])) {
+		
+		session_destroy();
+		
+		header("Location: login.php");
+		
+	}
+	
+	// ei ole tühjad väljad mida salvestada
+	if ( isset($_POST["gender"]) &&
+		 isset($_POST["age"]) &&
+		 isset($_POST["daynumber"]) &&
+		 isset($_POST["month"]) &&
+		 isset($_POST["year"]) &&
+		 isset($_POST["WorkoutHours"]) &&
+		 !empty($_POST["gender"]) &&
+		 !empty($_POST["age"]) &&
+		 !empty($_POST["daynumber"]) &&
+		 !empty($_POST["month"]) &&
+		 !empty($_POST["year"]) &&
+		 !empty($_POST["WorkoutHours"])
+	  ) {
+		
+		$gender=cleanInput($_POST["gender"]);
+
+		
+		savePeople($_POST["gender"], $_POST["age"], $_POST["daynumber"], $_POST["month"], $_POST["year"], $_POST["WorkoutHours"]);
+	}
+	
+	$people = getAllPeople();
+	
+	//echo "<pre>"; //nüüd näitab netilehel ilusamini andmeid, see rida ei ole tegelikult oluline
+	//var_dump($people);
+	//echo "</pre>";
+	
+?>
+<h1>Treeningu andmete sisestamine</h1>
+<p>
+	Tere tulemast <?=$_SESSION["email"];?>!
+	<a href="?logout=1">Logi välja</a>
+</p> 
+
+<h1>Salvesta andmed</h1>
+<form method="POST">
+			
+	<label>Sugu</label><br>
+	<input type="radio" name="gender" value="male" > Mees<br>
+	<input type="radio" name="gender" value="female" > Naine<br>
+	<input type="radio" name="gender" value="Unknown" > Ei oska öelda<br>
+	
+	<!--<input type="text" name="gender" ><br>-->
+	
+	<br><br>
+	<label>Vanus</label><br>
+	<input name="age" type="age"> 
+	
+	<br><br>
+	<label><h3>Kuupäev</h3></label>
+	<input name="Kuupäev" type="daynumber" placeholder="Kuupäev">
+	<input name="Kuu" type="month" placeholder="Kuu">
+	<input name="Aasta" type="year" placeholder="Aasta">
+	
+	<br><br>
+	<label><h3>Treeningu tunnid</h3></label>
+	<input name="WorkoutHours" type="workouthours">
+	
+	<br><br>
+	<input type="submit" value="Salvesta">
+	
+</form>
+
+	
---
+++ 2.kodutoo-I-ruhm/functions.php
@@ -0,0 +1,216 @@
+<?php
+	
+	// 2.kodutöö
+	//functions.php
+	require("../../config.php");
+	
+	//alustan sessiooni, et saaks kasutada
+	//$_SESSSION muutujaid
+	session_start();
+	
+	//********************
+	//****** SIGNUP ******
+	//********************
+	//$name = "Marliis";
+	//var_dump($GLOBALS);
+	
+	$database = "if16_Marliis";
+	
+	function signup ($name, $gender, $age, $email, $password) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"],$GLOBALS["database"]);
+
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (name, gender, age, email, password) VALUES (?, ?, ?, ?, ?)");
+		echo $mysqli->error;
+
+		//asendan küsimärgi väärtustega
+		//iga muutuja kohta tuleb kirjutada üks täht, mis tüüpi muutuja on
+		//s-stringi
+		//i-integer
+		//d-double/float
+
+		$stmt->bind_param("sisss", $name, $age, $email, $password, $gender);
+		
+		if ($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+			echo "ERROR ".$stmt->error;
+		}
+		
+	}
+	
+	
+	function login($email, $password) {
+		
+		$error = "";
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"],$GLOBALS["database"]);
+
+		$stmt = $mysqli->prepare("
+			SELECT id, email, password, created 
+			FROM user_sample
+			WHERE email = ?
+		");
+		echo $mysqli->error;
+		
+		//asendan küsimärgi
+		$stmt->bind_param("s", $email);
+		
+		//määran tupladele muutujad
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created);
+		$stmt->execute();
+		
+		//küsin rea andmeid
+		if($stmt->fetch()) {
+			//oli rida
+		
+			// võrdlen paroole
+			$hash = hash("sha512", $password);
+			if($hash == $passwordFromDb) {
+				
+				echo "kasutaja ".$id." logis sisse";
+				
+				
+				$_SESSION["userId"] = $id;
+				$_SESSION["email"] = $emailFromDb;
+				
+				//suunaks uuele lehele
+				header("Location: data.php");
+				exit();
+				
+			} else {
+				$error = "parool vale";
+			}
+			
+		
+		} else {
+			//ei olnud 
+			
+			$error = "sellise emailiga ".$email." kasutajat ei olnud";
+		}
+		
+		
+		return $error;
+		
+		
+	}
+	
+	
+	function savePeople ($gender, $age, $daynumber, $month, $year, $WorkoutHours) {
+		
+		//ühendus
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"],$GLOBALS["database"]);
+		// käsk
+		$stmt = $mysqli->prepare("INSERT INTO AthletesData (gender, age, daynumber, month, year, WorkoutHours, created) VALUES (?, ?, ?, ?, ?)");
+		echo $mysqli->error;
+
+		$stmt->bind_param("siiiii", $gender, $age, $daynumber, $month, $year, $WorkoutHours);
+		
+		if ($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+			echo "ERROR ".$stmt->error;
+		}
+		
+	}
+	
+	
+	
+	function getAllPeople () {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"],$GLOBALS["database"]);
+
+		//käsk
+		$stmt = $mysqli->prepare("SELECT id, gender, age, daynumber, month, year, WorkoutHours FROM AthletesData");
+		echo $mysqli->error;
+		
+		$stmt->bind_result($id, $gender, $age, $daynumber, $month, $year, $WorkoutHours);
+		$stmt->execute();
+		
+		// array("Marliis", "O")
+		$result = array();
+		
+		// seni kuni on üks rida andmeid saada (10 rida = 10 korda)
+		while ($stmt->fetch()) {
+			$person = new StdClass();
+			$person->id = $id;
+			$person->gender = $gender;
+			$person->age = $age;
+			$person->daynumber = $daynumber;
+			$person->month = $month;
+			$person->year = $year;
+			$person->WorkoutHours = $WorkoutHours;
+			
+			//echo $color."<br>";
+			array_push($result, $person);
+		}
+		
+	$stmt->close();
+	$mysqli->close();
+	
+	return $result;
+		
+	}
+	
+	
+	
+	
+	
+	function cleanInput($input){
+		
+		$input=trim($input);
+		$input=stripslashes($input);
+		$input=htmlspecialchars($input);
+		
+		return $input;
+	}
+	
+	
+	
+	
+	/*function sum ($x, $y) {
+		
+		return $x + $y;
+		
+	}
+	
+	function hello ($firstname, $lastname) {
+		
+		return "Tere tulemast ".$firstname." ".$lastname."!";
+		
+	}
+	
+	echo sum(5476567567,234234234);
+	echo "<br>";
+	$answer = sum(10,15);
+	echo $answer;
+	echo "<br>";
+	echo hello ("Marliis", "Odamus");
+	*/
+	
+	
+	/*
+	
+	function issetAndNotEmpty($var) {	
+		if ( isset ( $var ) ) {
+			if ( !empty ($var ) ) {
+				return true;			
+			}	
+		} 
+		
+		return false;	
+	}
+	
+	if (issetAndNotEmpty($_POST["loginEmail"])) {
+		
+		//vastab tõele
+		
+	}
+	
+	
+	
+	
+	*/
+
+
+?>
